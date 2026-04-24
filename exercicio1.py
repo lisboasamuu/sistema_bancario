ea9b9bc78b4fef1f01a89d6f94830f14da62a6b3
lisboasamuu <lisboasamuu@gmail.com>
2026-04-24 00:21:25 -0300

    Create Main and tranferir( )

--- exercicio1.py
+++ exercicio1.py
@@ -14,11 +14,13 @@ class JSONStorage:
             #metodo para serializar uma string em python para uma string em json
     @staticmethod
     def load(filename):
-        '''carregar os dados e retornar none se não existri'''
+        '''carregar os dados e retornar none se não existir'''
         if not os.path.exists(filename):
             return None
         with open(filename, 'r', encoding='utf-8') as f:
             return json.load(f)
+
+# -----------------------------------------
 # Classe Conta
 # -----------------------------------------
 class Conta:
@@ -34,7 +36,7 @@ class Conta:
             print('Ops, o valor não pode ser negativo!')
             return False
         self.saldo += valor
-        print(f'R${self.saldo:,.2f}')
+        print('Deposito realizado com sucesso.')
         self.historico.append(f'Depósito de +R${valor:,.2f} realizado')
         return True
 
@@ -47,7 +49,7 @@ class Conta:
             print('Saldo insuficiente')
             return False
         self.saldo -= valor
-        print(f'R${self.saldo:,.2f}')
+        print('Saque realizado com sucesso.')
         self.historico.append(f'Saque de -{valor:,.2f} realizado')
         return True
 
@@ -110,15 +112,54 @@ class Banco:
         self.salvar_dados()
 
     def obter_conta(self, cpf):
-        conta = self.contas.get(cpf)
-        if not conta:
-            print(f'Conta com {cpf} não encontrado')
+        return self.contas.get(cpf)
+
 
     def exibir_saldo(self, cpf):
         conta = self.obter_conta(cpf)
         if conta:
-            conta.exibir_saldo()       
-#
+            conta.exibir_saldo() 
+
+    def exibir_historico(self, cpf):
+        conta = self.obter_conta(cpf)
+        if conta:
+            conta.exibir_historico()
+
+    def transferir(self, cpf_origem, cpf_destino, valor):
+        modalidade = input('Qual a modalide do pagamento? ').strip()
+        if modalidade == '':
+            print('Informe a modalidade!')
+            return False        
+        #localiza as contas
+        conta_origem = self.obter_conta(cpf_origem)
+        if not conta_origem:
+            print('Conta origem não encontrada!')
+            return False
+
+        conta_destino = self.obter_conta(cpf_destino)
+        if not conta_destino:
+            print('Conta destino não encontrada!')
+            return False
+
+        #validação do dado de valor
+        if valor <= 0:
+            print('Ops, o valor não pode ser negativo!')
+            return False
+        if valor > conta_origem.saldo:
+            print('Saldo insuficiente')
+            return False
+        #realização da transferencia
+        conta_origem.saldo -= valor
+        conta_destino.saldo += valor
+        #salvamento do dado
+        self.salvar_dados()
+        #adicionando ao historico
+        conta_origem.historico.append(f'{modalidade} enviada para {cpf_destino} no valor de {valor}')
+        conta_destino.historico.append(f'{modalidade}a recebida de {cpf_origem} no valor de {valor}')
+
+        print(f'{modalidade} realizada com sucesso!')
+        print(f'De: {conta_origem.titular} para {conta_destino.titular}')
+
 # --- PERSISTENCIA DE DADOS 
 #             
     def salvar_dados(self):
@@ -126,7 +167,6 @@ class Banco:
         for cpf, conta in self.contas.items():
             dados[cpf] = conta.to_dict()
         JSONStorage.save(dados, self.arquivo_dados)
-        print('\nDados salvo com sucesso.')
 
     def carregar_dados(self):
         dados = JSONStorage.load(self.arquivo_dados)
@@ -149,7 +189,7 @@ def main():
         print("3️⃣  Sacar")
         print("4️⃣  Consultar saldo")
         print("5️⃣  Ver histórico de movimentações")
-        print("6️⃣  Listar todas as contas")
+        print("6️⃣  Transferir")
         print("0️⃣  Sair")
         print("=" * 50)
 
@@ -163,7 +203,23 @@ def main():
             banco.criar_conta(cpf, nome)
         elif opcao == '2':
             cpf = input('insira o CPF do titular: ').strip()
-            
+            valor = float(input('Insira o valor que deseja depositar: '))
+            banco.depositar(cpf, valor)
+        elif opcao == '3':
+            cpf = input('insira seu cpf: ').strip()
+            valor_saque = float(input('Insira o valor que deseja sacar: '))
+            banco.saque(cpf, valor_saque)
+        elif opcao == '4':
+            cpf = input('insira seu cpf da conta que deseja ver o saldo: ').strip()
+            banco.exibir_saldo(cpf)
+        elif opcao == '5':
+            cpf = input('insira seu cpf: ').strip()
+            banco.exibir_historico(cpf)
+        elif opcao == '6':
+            cpf_origem = input('Informe o CPF da conta de origem da transferência: ')
+            cpf_destinatario = input('Informe o CPF da conta de destino: ')
+            valor_transferencia = float(input('Informe o valor de transferência: ').replace(",", "."))
+            banco.transferir(cpf_origem, cpf_destinatario, valor_transferencia)
 
 if __name__ == "__main__":
     main()
